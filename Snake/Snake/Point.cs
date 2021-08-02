73af4efa06de889596af031becc287a57ea8fc7a
Ivan <vanbog@mail.ru>
2021-08-02 22:38:34 +0400

    created class Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
