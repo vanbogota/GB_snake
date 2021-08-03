f78ef9f908513812b26465b223f78616a2fcb150
Ivan <vanbog@mail.ru>
2021-08-03 23:08:20 +0400

    класс List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -11,6 +12,26 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');            
             p2.Draw();
+
+            List<char> chars = new List<char>();
+            chars.Add('*');
+            chars.Add('#');
+            chars.Add('+');
+            chars.Add('=');
+
+            Point p3 = new Point(7, 6, chars[2]);
+            p3.Draw();
+
+            Point p4 = new Point(8, 9, chars[^1]);
+            p3.Draw();
+
+            List<Point> points = new List<Point>
+            {
+                p1,
+                p2,
+                p3,
+                p4
+            };
 
             Console.ReadLine();
         }
